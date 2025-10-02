2bfb04c002f6a246144a33da1cc641d29c1c9113
Chindanai 66102122108 <chindanai.su66@snru.ac.th>
2025-10-02 19:00:41 +0700

    Finalize static fragment page and wiring from MainActivity

--- app/src/main/java/com/example/androidfragmenthw/A66102122108Activity.kt
+++ app/src/main/java/com/example/androidfragmenthw/A66102122108Activity.kt
@@ -1,47 +1,14 @@
 package com.example.androidfragmenthw
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidfragmenthw.ui.theme.AndroidFragmentHWTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class A66102122108Activity : ComponentActivity() {
+class A66102122108Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AndroidFragmentHWTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_66102122108)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidFragmentHWTheme {
-        Greeting("Android")
+        // โชว์ชื่อกิจกรรมบน Title Bar ให้ตรงตามโจทย์
+        title = "66102122108Activity"
     }
 }
