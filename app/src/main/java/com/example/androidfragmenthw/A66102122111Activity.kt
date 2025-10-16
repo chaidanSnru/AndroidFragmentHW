e5e26dabc93f53590705d2590130e24d0303ceca
chiratphatchara <chiratphatchara.tu66@snru.ac.th>
2025-10-16 21:36:22 +0700

    เพิ่ม Activity66102122111 และ Fragment แสดงรูปและชื่อ

---
+++ app/src/main/java/com/example/androidfragmenthw/A66102122111Activity.kt
@@ -0,0 +1,15 @@
+package com.example.androidfragmenthw 
+
+class A66102122131Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_66102122111)
+    }
+}
+
+class A66102122131Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_66102122111)
+    }
+}
