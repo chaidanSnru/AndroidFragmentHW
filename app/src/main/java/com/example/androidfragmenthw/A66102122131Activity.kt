1b8249a93cac12fdc51f8d965314685286a4b895
chiratphatchara <chiratphatchara.tu66@snru.ac.th>
2025-10-16 21:16:32 +0700

    เพิ่ม Activity และ Fragment แสดงข้อมูลนักศึกษา 66102122131

---
+++ app/src/main/java/com/example/androidfragmenthw/A66102122131Activity.kt
@@ -0,0 +1,8 @@
+package com.example.androidfragmenthw 
+
+class A66102122131Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_66102122122)
+    }
+}
