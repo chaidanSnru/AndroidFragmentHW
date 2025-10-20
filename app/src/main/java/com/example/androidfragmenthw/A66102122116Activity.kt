8cf8d96fd2f6a0c0cde599acb3c811a9c5e05230
nitipat116 <nitipat.ja66@snru.ac.th>
2025-10-20 12:51:59 +0700

    Add A66102122116Activity with static ProfileFragment and Thai TextView

---
+++ app/src/main/java/com/example/androidfragmenthw/A66102122116Activity.kt
@@ -0,0 +1,12 @@
+package com.example.androidfragmenthw
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class A66102122116Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_66102122116)
+        title = "A66102122116Activity"
+    }
+}
