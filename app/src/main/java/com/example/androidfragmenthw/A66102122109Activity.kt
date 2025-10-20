01c5925b9dd1d4dc2580febab6fc2c8e193fd165
watcharachai <watcharachai.na66@snru.ac.th>
2025-10-21 01:23:03 +0700

    นางสาวหัทยา จันทรังษี 66102122109

---
+++ app/src/main/java/com/example/androidfragmenthw/A66102122109Activity.kt
@@ -0,0 +1,13 @@
+package com.example.androidfragmenthw
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class A66102122109Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_66102122109)
+
+        title = "66102122109Activity"
+    }
+}
