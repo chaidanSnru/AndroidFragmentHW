cd758fa08450e4fdd0c0434d825af6ff74c8160f
nitipat116 <nitipat.ja66@snru.ac.th>
2025-10-20 13:36:57 +0700

    66102122112

---
+++ app/src/main/java/com/example/androidfragmenthw/A66102122112Activity.kt
@@ -0,0 +1,12 @@
+package com.example.androidfragmenthw
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class A66102122112Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_66102122112)
+        title = "A66102122112Activity"
+    }
+}
